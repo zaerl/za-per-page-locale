3f80e7bd7240bfb001c0a1c8edc7cde626f2bb99
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2024-04-02 21:58:03 +0200

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Per Page Locale ===
+Contributors: zaerl
+Tags: locale, i18n, l10n
+Requires at least: 4.4
+Tested up to: 6.5
+Stable tag: 0.1
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Allows you to select the site locale per page.
+
+== Description ==
+
+### Per Page Locale ###
+
+This free plugin offers a simple way to change the site locale per page.
+
+== Installation ==
+
+1. To begin, click on the Plugins link in the left hand sidebar, then click Add New.
+2. Search for "Per Page Locale". The latest version will be in the search results. Click the "Install Now" button.
+3. Next, click the "Activate" button.
+
+== Changelog ==
+### 0.1 - 2024-04-02
+- Created the plugin.
---
+++ za-per-page-locale.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * Plugin Name:  Per Page Locale
+ * Plugin URI:   https://zaerl.com/
+ * Description:  Allows you to change the locale per page.
+ * Version:      0.1
+ * Requires at least: 4.4
+ * Author:       Francesco Bigiarini
+ * Author URI:   https://zaerl.com/
+ * License:      GPLv2 or later
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:  za-per-page-locale
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/*
+ * Display the meta box
+ *
+ * @param WP_Post $post The post object
+ */
+function za_ppl_meta_box_callback( $post ) {
+	require_once ABSPATH . 'wp-admin/includes/translation-install.php';
+
+	$locale    = get_post_meta( $post->ID, 'za_ppl_locale', true );
+	$en_us     = array( 'en_US' => array( 'english_name' => 'English (United States)' ) );
+	$languages = array_merge( $en_us, wp_get_available_translations() );
+
+	echo '<select name="za_ppl_locale">';
+
+	foreach ( $languages as $code => $language ) {
+		printf( '<option value="%s"%s>%s</option>', esc_attr( $code ), selected( $locale, $code, false ), esc_html( $language['english_name'] ) );
+	}
+
+	echo '</select>';
+}
+
+/*
+ * Add a meta box to the post editor
+ */
+function za_ppl_add_meta_box() {
+	add_meta_box( 'za_ppl', __( 'Locale', 'za-per-page-locale' ), 'za_ppl_meta_box_callback', array( 'page', 'post' ), 'side' );
+}
+
+add_action( 'add_meta_boxes', 'za_ppl_add_meta_box' );
+
+/*
+ * Save the locale when the post is saved
+ *
+ * @param int $post_id The post ID
+ */
+function za_ppl_save_post( $post_id ) {
+	if ( array_key_exists( 'za_locale', $_POST ) ) {
+		update_post_meta( $post_id, 'za_ppl_locale', $_POST['za_locale'] );
+	}
+}
+
+add_action( 'save_post', 'za_ppl_save_post' );
+
+/*
+ * Filter the locale
+ *
+ * @param string $locale The current locale
+ * @return string The new locale
+ */
+function za_ppl_filter_locale( $locale ) {
+	if ( is_singular( array( 'post', 'page' ) ) ) {
+		$post_locale = get_post_meta( get_queried_object_id(), 'za_ppl_locale', true );
+
+		if ( $post_locale ) {
+			return $post_locale;
+		}
+	}
+	return $locale;
+}
+
+add_filter( 'locale', 'za_ppl_filter_locale' );
